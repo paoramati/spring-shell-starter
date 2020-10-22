255ce11a1f88ae59df7572c48e83debd924ed6d1
paoramati <brandonmitchellpaul@gmail.com>
2020-10-22 21:48:44 +1300

    Customisable prompt

--- src/main/java/au/com/bmp/springshellstarter/shell/AppPromptProvider.java
+++ src/main/java/au/com/bmp/springshellstarter/shell/AppPromptProvider.java
@@ -2,17 +2,21 @@ package au.com.bmp.springshellstarter.shell;
 
 import org.jline.utils.AttributedString;
 import org.jline.utils.AttributedStyle;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.shell.jline.PromptProvider;
 import org.springframework.stereotype.Component;
 
 @Component
 public class AppPromptProvider implements PromptProvider
 {
+
+    @Value("${bmp.shell.prompt}")
+    private String prompt;
+
     @Override
     public AttributedString getPrompt()
     {
-        return new AttributedString("AIS-CLI:>",
-                                    AttributedStyle.DEFAULT.foreground(AttributedStyle.BLUE)
+        return new AttributedString(prompt+":>", AttributedStyle.DEFAULT.foreground(AttributedStyle.BLUE)
         );
     }
 }
